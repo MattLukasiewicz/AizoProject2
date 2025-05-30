802ef57a38534a015f5d38dc564c2853117a1837
Mateusz <mateuszlukasiewicz04@gmail.com>
2025-05-30 21:41:36 +0200

    Działają algorytmy

--- main.cpp
+++ main.cpp
@@ -18,40 +18,41 @@ void generateAndSaveRandomGraph(int vertices, int edges, const std::string& file
         return;
     }
 
+    // Maksymalna liczba krawędzi w nieskierowanym grafie
+    int maxEdges = vertices * (vertices - 1) / 2;
+    if (edges > maxEdges) edges = maxEdges;
+
     outfile << vertices << " " << edges << "\n";
 
-    // 1) Przygotuj tablicę visited i lista istniejących krawędzi
-    bool* visited = new bool[vertices];
-    for (int i = 0; i < vertices; ++i) visited[i] = false;
+    // 1) Przygotuj visited[] i macierz exists[][] do śledzenia spójności i duplikatów
+    bool* visited = new bool[vertices]();
     visited[0] = true;
 
-    // Tablica do sprawdzania, czy krawędź istnieje
     bool** exists = new bool*[vertices];
     for (int i = 0; i < vertices; ++i) {
-        exists[i] = new bool[vertices];
-        for (int j = 0; j < vertices; ++j) exists[i][j] = false;
+        exists[i] = new bool[vertices]();
     }
 
-    srand(static_cast<unsigned int>(time(nullptr)));
+    srand(static_cast<unsigned>(time(nullptr)));
 
-    // 2) Generuj spójne drzewo rozpinające (n-1 krawędzi)
-    int added = 0;
-    while (added < vertices - 1) {
+    // 2) Generuj spójne drzewo rozpinające (vertices - 1 krawędzi)
+    int treeEdges = 0;
+    while (treeEdges < vertices - 1) {
         int u = rand() % vertices;
         int v = rand() % vertices;
         if (u != v && visited[u] != visited[v]) {
             int w = rand() % 100 + 1;
             outfile << u << " " << v << " " << w << "\n";
             exists[u][v] = exists[v][u] = true;
-            visited[v] = visited[u] = true;
-            ++added;
+            visited[u] = visited[v] = true;
+            ++treeEdges;
         }
     }
 
-    // 3) Dodaj pozostałe krawędzie
-    int toAdd = edges - (vertices - 1);
-    added = 0;
-    while (added < toAdd) {
+    // 3) Dodaj pozostałe krawędzie do osiągnięcia edges
+    int extra = edges - (vertices - 1);
+    int added = 0;
+    while (added < extra) {
         int u = rand() % vertices;
         int v = rand() % vertices;
         if (u != v && !exists[u][v]) {
@@ -64,12 +65,15 @@ void generateAndSaveRandomGraph(int vertices, int edges, const std::string& file
 
     // 4) Zwolnij pamięć
     delete[] visited;
-    for (int i = 0; i < vertices; ++i) delete[] exists[i];
+    for (int i = 0; i < vertices; ++i) {
+        delete[] exists[i];
+    }
     delete[] exists;
 
     outfile.close();
     std::cout << "Graf zapisano do pliku: " << filename << "\n";
 }
+
 
 
 void displayMenu() {
